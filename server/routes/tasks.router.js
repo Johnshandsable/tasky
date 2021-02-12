060834efb5aa1a3afa910ac918064d09c75ac98e
Johnshandsable <Jshands@asu.edu>
2021-02-12 12:45:28 -0600

    created a front end which allows user to create a task

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -5,4 +5,20 @@ const tasksRouter = express.Router();
 // DB CONNECTION
 const pool = require('../modules/pool');
 
+tasksRouter.get('/', (req, res) => {
+  console.log('SERVER - GET inside /tasks');
+});
+
+tasksRouter.post('/', (req, res) => {
+  console.log('SERVER - POST inside /tasks');
+});
+
+tasksRouter.put('/', (req, res) => {
+  console.log('SERVER - PUT inside /tasks');
+});
+
+tasksRouter.delete('/', (req, res) => {
+  console.log('SERVER - DELETE inside /tasks');
+});
+
 module.exports = tasksRouter;
