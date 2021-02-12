2e4c4df11bbdbbb168bd8c2e702ba7751d933651
Johnshandsable <Jshands@asu.edu>
2021-02-12 13:02:49 -0600

    added a get call on the client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,24 @@ console.log('JS loaded');
 
 $(document).ready(function () {
   console.log('jQuery loaded');
+
+  // EVENT HANDLERS GO HERE
+  $(document).on('click', '#btnAddTask', addTask);
 });
+
+function addTask(event) {
+  event.preventDefault();
+  console.log('inside addTask() ');
+
+  $.ajax({
+    url: '/tasks',
+    method: 'GET',
+  })
+    .then(function (response) {
+      // Expecting back an object of items to do
+      console.table('CLIENT - A response occurred', response);
+    })
+    .catch(function (error) {
+      console.log('CLIENT - An error occurred', error);
+    });
+}
